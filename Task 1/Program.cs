f3e633e55ca1ad1dd9371da06bfcd88d55d09830
PreetiMehta111 <preetikumarimehta267@gmail.com>
2025-11-12 11:10:04 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operators op = new Operators();
+
+            op.Add(10, 5);
+            op.Subtract(10, 5);
+            op.Multiply(10, 5);
+            op.Divide(10, 5);
+            op.OddEvenFinder(7);
+
+            Console.ReadLine(); // Keeps console open
+        }
+    }
+}
