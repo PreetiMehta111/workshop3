4bb027010dd33954d8c37ecd00029b473a9f7741
PreetiMehta111 <preetikumarimehta267@gmail.com>
2025-11-12 12:02:26 +0545

    Task 6

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Task_6
+﻿using System;
+
+class ErrorHandling
 {
-    internal class Program
+    public void ConvertInput()
     {
-        static void Main(string[] args)
+        try
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + number);
         }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number format");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed");
+        }
+    }
+
+    public void CheckPassword()
+    {
+        try
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+            else
+            {
+                Console.WriteLine("Password satisfied the requirement");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static void Main()
+    {
+        ErrorHandling obj = new ErrorHandling();
+        obj.ConvertInput();
+        obj.CheckPassword();
     }
 }
