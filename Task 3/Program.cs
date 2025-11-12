3084128059ecedb5f547780a7b53935f8e33334f
PreetiMehta111 <preetikumarimehta267@gmail.com>
2025-11-12 11:30:20 +0545

    Task 3

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Task_3
+﻿using System;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 13)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You are a child.");
+        }
+        else if (age >= 13 && age < 20)
+        {
+            Console.WriteLine("You are a teenager.");
+        }
+        else if (age >= 20 && age < 60)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are a senior citizen.");
         }
     }
 }
